1affe5e9d4276d4a110bf8f00eba0e0806cbf65a
Phuc Ho <hotrongphuc643@gmail.com>
2024-01-28 00:28:06 +0700

    update

--- main.py
+++ main.py
@@ -15,9 +15,12 @@ game_state = "start"
 bg = pygame.image.load("assets/images/background_1.png")
 bg_start = pygame.transform.scale(pygame.image.load("assets/images/bg_start.jpg"),(1200,670))
 my_font = pygame.font.Font('assets/fonts/ScaryZombie-2OoMv.ttf',64)
-start_game = my_font.render("Start Game",True,(255,255,255))
-start_game_hover = my_font.render("Start Game",True,(255,0,0))
+my_font_2 = pygame.font.Font('assets/fonts/ScaryZombie-2OoMv.ttf',40)
+start_game = my_font_2.render("Start Game",True,(255,255,255))
+start_game_hover = my_font_2.render("Start Game",True,(255,0,0))
 game_name = my_font.render("Whack A Zombie",True,(255,0,0))
+game_over = my_font.render("Game Over",True,(255,0,0))
+play_again = my_font_2.render("Play Again",True,(0,255,0))
 # bg = pygame.transform.scale(bg,(600,600))
 hammer = pygame.image.load("assets/animation/hammer.png")
 zombie = Zombie()
@@ -39,12 +42,26 @@ def drawHammer(screen,mx,my):
 
 def drawStartScreen(screen):
     screen.blit(bg_start,(0,0))
-    mx,my = pygame.mouse.get_pos()
-    if mx > 600-start_game.get_width()/2 and mx < 600+start_game.get_width()/2 and my > 250 and my < 250+start_game.get_height():
+    if checkMouseInButton(600-start_game.get_width()/2,250,start_game):
         screen.blit(start_game_hover,(600-start_game.get_width()/2,250))
     else:
         screen.blit(start_game,(600-start_game.get_width()/2,250))
     screen.blit(game_name,(600-game_name.get_width()/2,100))
+
+def drawGameOver(screen):
+    screen.blit(bg_start,(0,0))
+    screen.blit(game_over,(600-game_over.get_width()/2,150))
+    if checkMouseInButton(600-play_again.get_width()/2,250,play_again):
+        play_again.set_alpha(100)
+    else:
+        play_again.set_alpha(255)
+    screen.blit(play_again,(600-play_again.get_width()/2,250))
+
+def checkMouseInButton(x,y,button):
+    mx,my = pygame.mouse.get_pos()
+    if mx > x and mx < x+button.get_width() and my > y and my < y+button.get_height():
+        return True
+    return False
 
 def start_to_run():
     global game_state
@@ -72,10 +89,18 @@ while running:
                 else:
                     ui.increase_miss()
                     sound.playMiss()
-            else:
+            elif game_state == "start":
                 mx, my = pygame.mouse.get_pos()
-                if mx > 600-start_game.get_width()/2 and mx < 600+start_game.get_width()/2 and my > 250 and my < 250+start_game.get_height():
+                if checkMouseInButton(600-start_game.get_width()/2,250,start_game):
                     start_to_run()
+            elif game_state == "over":
+                if checkMouseInButton(600-play_again.get_width()/2,250,play_again):
+                    game_state = "run"
+                    pygame.mouse.set_visible(False)
+                    ui.miss = 0
+                    ui.hit = 0
+                    sound.start.stop()
+                    sound.playBGMusic()
 
     if game_state == "start":
         drawStartScreen(screen)
@@ -88,6 +113,15 @@ while running:
             ui.draw_timer_bar(screen,zombie.time_out_hole)
         mx,my = pygame.mouse.get_pos()
         drawHammer(screen,mx,my)
+        if ui.miss == 10:
+            game_state = "over"
+            sound.background_music.stop()
+            sound.stunned.stop()
+            sound.playStart()
+            pygame.mouse.set_visible(True)
+    elif game_state == "over":
+        drawGameOver(screen)
+        ui.draw(screen)
     
     pygame.display.flip()
     clock.tick(60) 
